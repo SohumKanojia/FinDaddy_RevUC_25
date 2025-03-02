13352030508f3e5d03c0d5e17bfe88039c0bbc6b
Sohum Kanojia <kanojisr@mail.uc.edu>
2025-03-02 10:44:27 -0500

    Removed redundant files

--- help.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-client = genai.Client(api_key="AIzaSyBXalr25Xvh2LQbzbUEwvIEfA2hvUNbosA")
-while(True):
-    query = input("Enter your query: ")
-    response = client.models.generate_content(
-        model="gemini-2.0-flash", contents = query
-    )
-    print(response.text)
